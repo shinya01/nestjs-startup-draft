45b4ee4eeb131537fd72f3fb3e1e359b395ae4e8
skudo <skudo.xcube@gmail.com>
2026-01-23 09:02:56 +0000

    devcontainer環境のみ

--- src/common/repositories/index.ts
+++
@@ -1,5 +0,0 @@
-// common/repositories/index.ts
-import { UserRepository } from './user.repository';
-
-export const REPOSITORIES = [UserRepository]; // Add other repositories as needed
-export { UserRepository }; // Export other repositories as needed
