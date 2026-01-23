2ec43e5dc90bb4a41727af234b1a745e017e1f43
skudo <skudo.xcube@gmail.com>
2026-01-23 12:40:25 +0000

    構築内容コミット

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+// 方法①（推奨）: 自動で読み込む
+import 'dotenv-flow/config';
+
+// 方法②: 明示的に読み込む
+// import * as dotenvFlow from 'dotenv-flow';
+// dotenvFlow.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
 bootstrap();
